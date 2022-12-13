c546782faffce8a31a4b6510f58d161d31b06f49
Amirali87Ahmadi <amirali97246@gmail.com>
2022-12-13 22:14:53 +0330

    AmiraliAhmadi

---
+++ Android.js
@@ -0,0 +1,15 @@
+var q = document.getElementById("cheshm1");
+var p = document.getElementById("cheshm2");
+var a = document.getElementById("badan");
+var s = document.getElementById("sar");
+var m = document.getElementById("dast2");
+var n = document.getElementById("dast1");
+
+a.onclick = function(){
+    p.style.backgroundColor = "lawngreen";
+    q.style.backgroundColor = "lawngreen";
+}
+s.onclick = function(){
+    p.style.backgroundColor = "white";
+    q.style.backgroundColor = "white";
+}
